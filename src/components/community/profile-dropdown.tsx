fe9529a76fac0415290b030d1a0582e50f29a28c
Ktumsh <jbz.january@gmail.com>
2024-08-06 23:33:35 -0400

    fix theme toggle

--- src/components/community/profile-dropdown.tsx
+++ src/components/community/profile-dropdown.tsx
@@ -64,7 +64,7 @@ export default function ProfileDropdown({ name, username, avatar }: Props) {
         <DropdownMenu aria-label="Acciones del perfil" variant="flat">
           <DropdownItem
             key="profile"
-            textValue="Profile"
+            textValue="Perfil"
             href="/comunidad/perfil"
             className="rounded-xl data-[hover=true]:bg-gray-200 dark:data-[hover=true]:bg-base-dark text-base-color-h dark:text-base-color-dark-m data-[hover=true]:text-base-color-h dark:data-[hover=true]:text-base-color-dark !duration-150"
           >
@@ -73,29 +73,24 @@ export default function ProfileDropdown({ name, username, avatar }: Props) {
           <DropdownItem
             className="rounded-xl data-[hover=true]:bg-gray-200 dark:data-[hover=true]:bg-base-dark text-base-color-h dark:text-base-color-dark-m data-[hover=true]:text-base-color-h dark:data-[hover=true]:text-base-color-dark !duration-150"
             key="configurations"
-            textValue="Configurations"
+            textValue="Configuración"
           >
             Configuración
           </DropdownItem>
           <DropdownItem
             className="rounded-xl data-[hover=true]:bg-gray-200 dark:data-[hover=true]:bg-base-dark text-base-color-h dark:text-base-color-dark-m data-[hover=true]:text-base-color-h dark:data-[hover=true]:text-base-color-dark !duration-150"
             key="help_and_feedback"
-            textValue="Help and Feedback"
+            textValue="Centro de Ayuda"
           >
             Centro de Ayuda
           </DropdownItem>
           <DropdownItem
             isReadOnly
-            endContent={
-              <ThemeToggle
-                buttonClass="bg-gray-200 dark:bg-base-dark"
-                spanClass="dark:bg-base-full-dark"
-              />
-            }
+            endContent={<ThemeToggle />}
             className="rounded-xl data-[hover=true]:bg-gray-200 dark:data-[hover=true]:bg-base-dark text-base-color-h dark:text-base-color-dark-m data-[hover=true]:text-base-color-h dark:data-[hover=true]:text-base-color-dark"
-            textValue="Help and Feedback"
+            textValue="Tema"
           >
-            Cambiar tema
+            Tema
           </DropdownItem>
           <DropdownItem
             id="avatar_logout"
@@ -110,7 +105,6 @@ export default function ProfileDropdown({ name, username, avatar }: Props) {
           </DropdownItem>
         </DropdownMenu>
       </Dropdown>
-      <ThemeToggle buttonClass="hidden" />
     </div>
   );
 }
